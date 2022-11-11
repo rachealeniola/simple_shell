a73ce8c63f7b35d901eb2fd5f61a2798f4bceedb
root <rachealeniola92@gmail.com>
2022-11-11 12:54:06 -0800

    trial

---
+++ shelly/README.md
@@ -0,0 +1,3 @@
+writing a C programming to perform the functions of shell
+
+It's team work between RachealEniola & Andrea Ozuem.
---
+++ shelly/shellyy.h
@@ -0,0 +1,15 @@
+#ifndef shellyy_h
+#define shellyy_h
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char *readline(void);
+
+#endif
+
+
